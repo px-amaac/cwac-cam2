2635936759e5a63ba68c4be62761bdeca35ee4c8
Mark Murphy <mmurphy@commonsware.com>
2015-05-01 15:50:35 -0400

    stub implementation

---
+++ cam2/src/main/java/com/commonsware/cwac/cam2/CameraFragment.java
@@ -0,0 +1,53 @@
+/***
+ Copyright (c) 2015 CommonsWare, LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License"); you may
+ not use this file except in compliance with the License. You may obtain
+ a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+package com.commonsware.cwac.cam2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment for displaying a camera preview, with hooks to allow
+ * you (or the user) to take a picture.
+ */
+public class CameraFragment extends Fragment {
+  /**
+   * Standard fragment entry point.
+   *
+   * @param savedInstanceState State of a previous instance
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setRetainInstance(true);
+  }
+
+  /**
+   * Standard callback method to create the UI managed by
+   * this fragment.
+   *
+   * @param inflater Used to inflate layouts
+   * @param container Parent of the fragment's UI (eventually)
+   * @param savedInstanceState State of a previous instance
+   * @return the UI being managed by this fragment
+   */
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return(new View(getActivity()));
+  }
+}
